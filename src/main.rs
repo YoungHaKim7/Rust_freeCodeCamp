19dd7e413c2829702086789d7c18cb3c42ff8d2c
YoungHa Kim <ytok1108@aol.com>
2022-01-15 22:29:03 +0900

    self.inner.next

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,16 @@ use std::env::{args, Args};
 fn main() {
     let mut args: Args = args();
 
-    let first = args.nth(0);
-    println!("{:?}", first);
+    let first = args.nth(1).unwrap();
+    let operator = args.nth(2).unwrap();
+    let second = args.nth(3).unwrap();
+    println!("{:?} {} {}", first, operator, second);
+}
+
+fn nth(&mut self, n: usize) -> Option<String> {
+    // assume n = 0;
+    // inner = ["1", "2"]
+    self.inner.next() // "1"
+    // Calling next again results in second element
+    self.inner.next() // "2"
 }
